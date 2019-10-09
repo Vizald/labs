fd9847cccbdff3f37b8470c8c6e9595456f76970
Vizald <SecondVN@mail.ru>
2019-10-09 07:54:54 +0000

    labs

--- lab1.py
+++ lab1.py
@@ -1,16 +1,16 @@
 from math import *
 
-a = float(input())
-x = float(input())
+a = float(input("Введите a:"))
+x = float(input("Введите x:"))
 g = - (16 * a * a + 24 * a * x - 27 * x * x) / (45 * a * a - 29 * a * x + 4 * x * x)
-print(g)
+print("G =",g)
 
-a = float(input())
-x = float(input())
+a = float(input("Введите a:"))
+x = float(input("Введите x:"))
 f = -atan(10 * a * a + 13 * a * a - 30 * x * x)
-print(f)
+print("F =",f)
 
-a = float(input())
-x = float(input())
+a = float(input("ВВедите a:"))
+x = float(input("Введите x:"))
 y = log(2 * a * a + 19 * a * x + 9 * x * x + 1) / log(10)
-print(y)
+print("Y =",y)
