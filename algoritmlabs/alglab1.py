9e0e8721f689afe40aab1affc45fadb3acae2172
Vizald <56064927+Vizald@users.noreply.github.com>
2019-10-31 21:38:44 +0300

    Update alglab1.py

--- algoritmlabs/alglab1.py
+++ algoritmlabs/alglab1.py
@@ -8,6 +8,7 @@ symmmax = 0
 for i in range(n):
     array.append(randint(1, 999))
     print(array[i], end=" ")
+print()
 
 for i in range(n - 1):
     for j in range(n - i - 1):
@@ -15,7 +16,7 @@ for i in range(n - 1):
             per = array[j]
             array[j] = array[j + 1]
             array[j + 1] = per
-
+            
 for i in range(n):
     print(array[i], end=" ")
 print()
