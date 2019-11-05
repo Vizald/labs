882692ae4d1a499321e8df3e283f2ca10ab0aab3
Vizald <56064927+Vizald@users.noreply.github.com>
2019-11-05 19:56:00 +0300

    Update lab2.c

--- clabs/lab2.c
+++ clabs/lab2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
+int main(void) 
+{
     float a, x, g, f, y;
     int number;
 
@@ -17,6 +18,7 @@ int main(void) {
             scanf("%f", &x);
             g = - (16 * a * a + 24 * a * x - 27 * x * x) / (45 * a * a - 29 * a * x + 4 * x * x);
             printf("G=%f\n\n", g);
+            break;
         case 2:
             printf("Vvedite a: ");
             scanf("%f", &a);
@@ -24,6 +26,7 @@ int main(void) {
             scanf("%f", &x);
             f = -atan(10 * a * a + 13 * a * a - 30 * x * x);
             printf("F=%f\n\n", f);
+            break;
         case 3:
             printf("Vvedite a: ");
             scanf("%f", &a);
@@ -33,6 +36,7 @@ int main(void) {
             {
                 y = log(2 * a * a + 19 * a * x + 9 * x * x + 1) / log(10);
                 printf("Y=%f\n\n", y);
+                break;
             }
             else
             {
