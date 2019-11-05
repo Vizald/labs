7a7fd305b33fb9707c71fae75649059019781bac
Vizald <56064927+Vizald@users.noreply.github.com>
2019-11-05 20:55:05 +0300

    Update lab1.py

--- pylabs/lab1.py
+++ pylabs/lab1.py
@@ -3,14 +3,14 @@ from math import *
 a = float(input("Введите a:"))
 x = float(input("Введите x:"))
 g = - (16 * a ** 2 + 24 * a * x - 27 * x ** 2) / (45 * a ** 2 - 29 * a * x + 4 * x ** 2)
-print("G =", g)
+print("G =", g, end="")
 
 a = float(input("Введите a:"))
 x = float(input("Введите x:"))
 f = -atan(10 * a ** 2 + 13 * a * x - 30 * x ** 2)
-print("F =", f)
+print("F =", f, end="")
 
 a = float(input("ВВедите a:"))
 x = float(input("Введите x:"))
 y = log(2 * a ** 2 + 19 * a * x + 9 * x ** 2 + 1) / log(10)
-print("Y =", y)
+print("Y =", y, end="")
